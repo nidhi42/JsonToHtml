29ca8c5b599324fd52992776e975e31cdea05dbe
nidhi bhatt <Nidhibhatt@ADMINRG-6MKE54P>
2020-03-02 12:18:46 +0530

    added selectbox and textarea in json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
-import { HttpErrorResponse } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent implements OnInit {
   title = 'jsonToHtml';
   constructor(private httpService: HttpClient) { }
   arrLogin: any = [];
   showError: boolean;
-  ngOnInit() {
+   // tslint:disable-next-line:variable-name
+   form_template: any = [
+    {
+      type: 'select',
+      label: 'favorite book',
+      options: ['Jane Eyre', 'Pride and Prejudice', 'Wuthering Heights']
+    }
+  ];
+ngOnInit() {
     this.httpService.get('./assets/authentication.json').subscribe(
       data => {
         this.arrLogin = data;
       });
-
   }
-  onSubmit() {
+onSubmit() {
     this.arrLogin.forEach(element => {
       if (element.required && element.value === '') {
           this.showError = true;
        }
-      console.log(element);
     });
   }
 }
